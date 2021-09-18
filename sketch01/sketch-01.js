0b62a39e8905d40da27b20dbeb5bf9b2e395d1f1
Manojlovic1998 <manojlovic911@hotmail.com>
2021-09-18 10:13:51 +0200

    Format the source code

--- sketch01/sketch-01.js
+++ sketch01/sketch-01.js
@@ -1,50 +1,50 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+    dimensions: [1080, 1080]
 };
 
 
-const degToRad = (degrees) =>{
-  return degrees / 180 * Math.PI;
+const degToRad = (degrees) => {
+    return degrees / 180 * Math.PI;
 };
 
 
 const sketch = () => {
-  return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
+    return ({context, width, height}) => {
+        context.fillStyle = 'white';
+        context.fillRect(0, 0, width, height);
 
-    // Position of the drawing axis
-    const xPosition = width * 0.5;
-    const yPosition = height * 0.5;
-    // Dimension of the object
-    const w = width * 0.01;
-    const h = height * 0.1;
+        // Position of the drawing axis
+        const xPosition = width * 0.5;
+        const yPosition = height * 0.5;
+        // Dimension of the object
+        const w = width * 0.01;
+        const h = height * 0.1;
 
-    const objNum = 12;
-    const radius = width * 0.3;
-    let x, y;
+        const objNum = 12;
+        const radius = width * 0.3;
+        let x, y;
 
-    for (let i = 0; i <= objNum; i++) {
-      // Trigonometry
-      const slice = degToRad(360 / objNum);
-      const angle = slice * i;
+        for (let i = 0; i <= objNum; i++) {
+            // Trigonometry
+            const slice = degToRad(360 / objNum);
+            const angle = slice * i;
 
-      x = xPosition + radius * Math.sin(angle);
-      y = yPosition + radius * Math.cos(angle);
-      // Transforming and Rotating
-      context.save();
-      context.translate(x, y);
-      context.rotate(-angle);
+            x = xPosition + radius * Math.sin(angle);
+            y = yPosition + radius * Math.cos(angle);
+            // Transforming and Rotating
+            context.save();
+            context.translate(x, y);
+            context.rotate(-angle);
 
-      context.fillStyle = 'black';
-      context.beginPath();
-      context.rect(-w * 0.5, -h * 0.5, w, h);
-      context.fill();
-      context.restore();
-    }
-  };
+            context.fillStyle = 'black';
+            context.beginPath();
+            context.rect(-w * 0.5, -h * 0.5, w, h);
+            context.fill();
+            context.restore();
+        }
+    };
 };
 
 canvasSketch(sketch, settings);
