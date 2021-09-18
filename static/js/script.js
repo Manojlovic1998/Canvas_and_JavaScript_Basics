7858ced2ef19719f16d3fd92af92383bfddb0c64
Manojlovic1998 <manojlovic911@hotmail.com>
2021-09-18 08:08:05 +0200

    Use math.random method to draw squares on canvas

--- static/js/script.js
+++ static/js/script.js
@@ -25,7 +25,7 @@ for (let i = 0; i < 5; i++) {
         canvasContext.beginPath();
         canvasContext.rect(xPosition, yPosition, width, height);
         canvasContext.stroke();
-        if (i > 0 && i < 4) {
+        if (Math.random() > 0.5) {
             canvasContext.beginPath();
             canvasContext.rect(xPosition + 8, yPosition + 8, width - 16, height - 16);
             canvasContext.stroke();
