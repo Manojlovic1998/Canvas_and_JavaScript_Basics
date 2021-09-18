aa771fe88261beae9f35aa2fd2cc9c69330e6c6d
Manojlovic1998 <manojlovic911@hotmail.com>
2021-09-18 07:44:09 +0200

    Draw squares using for loop

--- static/js/script.js
+++ static/js/script.js
@@ -5,10 +5,25 @@ let canvasContext = canvas.getContext("2d");
 canvasContext.lineWidth = 4;
 canvasContext.beginPath();
 canvasContext.rect(100, 100, 400, 400);
-canvasContext.stroke();
-
+// canvasContext.stroke();
 
 
 canvasContext.beginPath();
-canvasContext.arc(300, 300, 100, 0, 2*Math.PI);
-canvasContext.stroke();
+canvasContext.arc(300, 300, 100, 0, 2 * Math.PI);
+// canvasContext.stroke();
+
+
+// Draw using loop
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+        let gap = 20;
+        let width = 60;
+        let height = 60;
+        let xPosition = 100 + (width + gap) * i;
+        let yPosition = 100 + (height + gap) * j;
+
+        canvasContext.beginPath();
+        canvasContext.rect(xPosition, yPosition, width, height);
+        canvasContext.stroke();
+    }
+}
