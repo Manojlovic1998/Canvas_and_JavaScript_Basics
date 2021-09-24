af6a8a6c6dc930d3d58a9526e439202dec7287ec
Manojlovic1998 <manojlovic911@hotmail.com>
2021-09-24 15:19:38 +0200

    Add noise and animate the sketch

--- sketch03/sketch-03.js
+++ sketch03/sketch-03.js
@@ -1,16 +1,19 @@
 const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
+const math = require('canvas-sketch-util/math');
 
 const settings = {
-    dimensions: [1080, 1080]
+    dimensions: [1080, 1080],
+    animate: true,
 };
 
 const sketch = () => {
-    return ({context, width, height}) => {
+    return ({context, width, height, frame}) => {
         context.fillStyle = 'white';
         context.fillRect(0, 0, width, height);
 
-        const cols = 4;
-        const rows = 3;
+        const cols = 10;
+        const rows = 10;
         const numCells = cols * rows;
 
         const gridWidth = width * 0.8;
@@ -20,6 +23,7 @@ const sketch = () => {
 
         const marginX = (width - gridWidth) * 0.5;
         const marginY = (height - gridHeight) * 0.5;
+
 
         for (let i = 0; i < numCells; i++) {
             let col = i % cols; // 0 1 2 3 0 1 2 3 for each iteration
@@ -31,10 +35,18 @@ const sketch = () => {
             const w = cellWidth * 0.8;
             const h = cellHeight * 0.8;
 
+            const n = random.noise2D(x + frame * 10, y,0.001);
+            const angle = n * Math.PI * 0.2;
+            const scale = math.mapRange(n, -1, 1, 1, 30);
+
+
             context.save();
             context.translate(x, y);
             context.translate(marginX, marginY);
             context.translate(cellWidth * 0.5, cellHeight * 0.5);
+            context.rotate(angle);
+
+            context.lineWidth = scale;
 
             context.beginPath();
             context.moveTo(w * -0.5, 0);
